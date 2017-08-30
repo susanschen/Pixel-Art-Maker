000e664461c1c710aae78adb26e87e123f72fcdd
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-08-30 18:22:49 -0400

    Get input height and width

--- designs.js
+++ designs.js
@@ -1,7 +1,13 @@
 // Select color input
+
 // Select size input
+let height = document.getElementById("input_height");
+console.log(height.value);
+let width = document.getElementById("input_width");
+console.log(width.value);
 
 // When size is submitted by the user, call makeGrid()
+
 
 function makeGrid() {
 
