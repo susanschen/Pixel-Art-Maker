13385643e56759f21f6a45462a78a8cc904fd645
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-08-30 19:43:16 -0400

    Add eventListener to Submit

--- designs.js
+++ designs.js
@@ -7,10 +7,13 @@ let width = document.getElementById("input_width");
 console.log(width.value);
 
 // When size is submitted by the user, call makeGrid()
-
+let submitType = document.getElementsByTagName("input")[2];
+console.log(submitType);
+submitType.addEventListener("click", makeGrid);
 
 function makeGrid() {
 
 // Your code goes here!
+    console.log("grid");
 
 }
