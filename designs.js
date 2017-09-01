b9c79f8bc2513d9cd0b69ad837878e62d7ad1e6c
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-09-01 11:22:10 -0400

    Remove comments and logs

--- designs.js
+++ designs.js
@@ -1,14 +1,11 @@
-// Select inputs
 let canvas = document.getElementById("pixel_canvas");
 let height = document.getElementById("input_height");
 let width = document.getElementById("input_width");
 let sizePicker = document.getElementById("sizePicker");
 let color = document.getElementById("colorPicker");
 
-color.addEventListener("click", function(){console.log("color clicked");});
+color.addEventListener("click", function(){});
 
-// When size is submitted by the user, call makeGrid()
-// http://mycodingtricks.com/javascript/submit-form-using-javascript-ajax/
 sizePicker.onsubmit = function(event){
     event.preventDefault();
     clearGrid();
@@ -27,14 +24,12 @@ function makeGrid() {
     }
 }
 
-// https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
 function clearGrid(){
     while (canvas.firstChild){
          canvas.removeChild(canvas.firstChild);
     }
 }
 
-// Fill a square with the current color
 function fillSquare(){
     this.setAttribute("style", `background-color: ${color.value}`);
 }
